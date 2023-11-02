93d4a985889b0b03815279f04eea07af7d6b67f3
Jason <123558105+JChow999@users.noreply.github.com>
2023-11-02 17:40:21 -0400

    Adjust index to include operators and restructure Modify style to calculator more visually pleasing Add new functions to javascript for some operators Javascript file now supports keystrokes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,36 @@
-let num1 = 0;
-let num2 = 0;
+let operand1 = '';
+let operand2 = '';
 let operator = '';
-let target = 0;
+let endVal = '';
 
+const expression = document.querySelector('#expression');
+const numDisplay = document.querySelector('#numDisplay');
+const numBtn = document.querySelectorAll('.numBtn')
+
+document.onkeydown = function (e) {
+    const keyIsNum = isFinite(e.key);
+    if (keyIsNum) {
+        operand1 += e.key;
+        updateDisplay(numDisplay, operand1)
+    }
+}
+
+function removeChar() {
+    return
+}
+
+function clear() {
+    operator = ""
+    expression = ""
+}
+
+function updateDisplay(display, info) {
+    display.innerHTML = info;
+}
+
+function toggleNeg() {
+    if (operand1) return
+}
 
 function add(a, b) {
     return a + b;
@@ -17,6 +45,27 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b;
+}
+
+function operate(a, b) {
+    switch(operator) {
+        case '/':
+            return num1 = divide(a, b);
+        case '%':
+            return num1 = modulo(a, b);
+        case '*':
+            return num1 = multiply(a, b);
+        case '+':
+            return num1 = add(a, b);
+        case '-':
+            return num1 = subtract(a, b);
+        default:
+            alert("An error has occurred.")
+    }
 }
 
