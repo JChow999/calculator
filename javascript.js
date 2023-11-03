a9a59e5d590fdd3c0da1a08af67eb96b71637053
Jason <123558105+JChow999@users.noreply.github.com>
2023-11-03 18:54:58 -0400

    Adjust index to restructure calculator Adjust javascript and make adjustments to give calculator functionality

--- javascript.js
+++ javascript.js
@@ -1,84 +1,102 @@
-let operand1 = '0';
-let operand2 = '';
-let operator = '';
-let endVal = '';
-let expression = [];
+const MAX_LENGTH = 12;
 
 const expressionDisp = document.querySelector('#expression');
 const numDisplay = document.querySelector('#numDisplay');
-const numBtn = document.querySelectorAll('.numBtn')
+const btns = document.querySelectorAll('button')
+
+let numDispLrg = '0';
+let expression = [];
+
+
+btns.forEach(button => 
+    button.addEventListener('click', () => updateNumDisp(button.textContent)))
 
 document.onkeydown = function (e) {
-    if (e.key == 'Backspace') {
-        if (operand1.length <= 1) {
-            operand1 = '0';
+    updateNumDisp(e.key)
+}
+
+function updateNumDisp(key) {
+    console.log(key)
+    if (key == 'Backspace') {
+        if (numDispLrg.length <= 1) {
+            numDispLrg = '0';
         } else {
-            operand1 = operand1.slice(0, operand1.length - 1);
+            numDispLrg = numDispLrg.slice(0, numDispLrg.length - 1);
         }
-    } else if (isFinite(e.key)) {
-        if (operand1.charAt(0) == 0) {
-            operand1 = e.key;
+    } else if (key == '+/-') {
+        if (numDispLrg.charAt(0)== '0') {
+            return;
+        } else if (numDispLrg.charAt(0) == '-') {
+            numDispLrg = numDispLrg.slice(1)
         } else {
-            operand1 += e.key;  
+            numDispLrg = '-' + numDispLrg;
         }
+    } else if (isFinite(key)) {
+        if (numDispLrg.charAt(0) == 0 && numDispLrg.length < 2) {
+            numDispLrg = key;
+        } else {
+            numDispLrg += key;  
+        }
+    } else if (key == '.') {
+        if (numDispLrg.indexOf('.') == -1) {
+            numDispLrg += key;
+        }
+    } else if (/[+\-\*/%]/.test(key)) {
+        expression.push(numDispLrg);
+        expression.push(key);
+        numDispLrg = "0";
+    } else if (key == 'AC') {
+        expression = [];
+        numDispLrg = "0";
+    } else if (key == '=' || key == 'Enter') {
+        expression.push(numDispLrg);
+        operate();
+        numDispLrg = '0';
     }
-    updateDisplay(numDisplay, operand1)
-}
-
-function removeChar() {
-    return
-}
-
-function clear() {
-    operator = ""
-    expression = ""
+    
+    // For updating the displays
+    if (expression.length == 0) {
+        updateDisplay(expressionDisp, '0');
+    } else {
+        updateDisplay(expressionDisp, expression.join(" "))
+    }
+    updateDisplay(numDisplay, numDispLrg)
 }
 
 function updateDisplay(display, info) {
     display.innerHTML = info;
 }
 
-function toggleNeg() {
-    if (operand1) return
-}
+function operate() {
+    const specialChar = ['/', '%', '*', '+', '-']
+    
 
-function add(a, b) {
-    return a + b;
-}
+    for (let n = 0; n < specialChar.length; n++) {
+        while (expression.indexOf(specialChar[n]) != -1) {
+            let index = expression.indexOf(specialChar[n])
+            
+            let oper1 = expression[index - 1];
+            let oper2 = expression[index + 1]
+            let calcOperator = expression[index];
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function modulo(a, b) {
-    return a % b;
-}
-
-function operate(a, b) {
-
-
-
-    switch(operator) {
-        case '/':
-            return num1 = divide(a, b);
-        case '%':
-            return num1 = modulo(a, b);
-        case '*':
-            return num1 = multiply(a, b);
-        case '+':
-            return num1 = add(a, b);
-        case '-':
-            return num1 = subtract(a, b);
-        default:
-            alert("An error has occurred.")
+            endVal = calculate(parseFloat(oper1), calcOperator, parseFloat(oper2))
+            console.log(endVal)
+            expression.splice((index-1), 3, endVal)
+        }
     }
 }
 
+function calculate(operandOne, operator, operandTwo) {
+    switch (operator) {
+        case '+':
+            return operandOne + operandTwo;
+        case '-':
+            return operandOne - operandTwo;
+        case '*':
+            return operandOne * operandTwo;
+        case '/':
+            return operandOne / operandTwo;
+        case '%':
+            return operandOne % operandTwo
+    }
+}
