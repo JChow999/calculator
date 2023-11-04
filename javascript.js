6b605d26ffe2021e889c71d561be917ad5475bc7
Jason <123558105+JChow999@users.noreply.github.com>
2023-11-04 10:18:46 -0400

    Chage NumDisp logic to debug array issues

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const expressionDisp = document.querySelector('#expression');
 const numDisplay = document.querySelector('#numDisplay');
 const btns = document.querySelectorAll('button')
 
-let numDispLrg = '0';
+let numDispVal = '';
 let expression = [];
 
 
@@ -16,60 +16,78 @@ document.onkeydown = function (e) {
 }
 
 function updateNumDisp(key) {
-    console.log(key)
-    if (key == 'Backspace') {
-        if (numDispLrg.length <= 1) {
-            numDispLrg = '0';
+    if (key == 'Backspace' || key == 'Del') {
+        if (numDispVal.length <= 1) {
+            numDispVal = '';
         } else {
-            numDispLrg = numDispLrg.slice(0, numDispLrg.length - 1);
+            numDispVal = numDispVal.slice(0, numDispVal.length - 1);
         }
     } else if (key == '+/-') {
-        if (numDispLrg.charAt(0)== '0') {
+        if (numDispVal.charAt(0)== '0') {
             return;
-        } else if (numDispLrg.charAt(0) == '-') {
-            numDispLrg = numDispLrg.slice(1)
+        } else if (numDispVal.charAt(0) == '-') {
+            numDispVal = numDispVal.slice(1)
         } else {
-            numDispLrg = '-' + numDispLrg;
+            numDispVal = '-' + numDispVal;
         }
     } else if (isFinite(key)) {
-        if (numDispLrg.charAt(0) == 0 && numDispLrg.length < 2) {
-            numDispLrg = key;
+        if (numDispVal.charAt(0) == 0 && numDispVal.length < 2) {
+            numDispVal = key;
         } else {
-            numDispLrg += key;  
+            numDispVal += key;  
         }
     } else if (key == '.') {
-        if (numDispLrg.indexOf('.') == -1) {
-            numDispLrg += key;
+        if (numDispVal.indexOf('.') == -1) {
+            numDispVal += key;
         }
     } else if (/[+\-\*/%]/.test(key)) {
-        expression.push(numDispLrg);
-        expression.push(key);
-        numDispLrg = "0";
+        if (expression.length < 1 || numDispVal != "") {
+            expression.push(numDispVal);
+            expression.push(key)
+        } else if (expression.length == 1 && numDispVal == "") {
+            expression.push(key)
+        } 
+
+        if (/[+\-\*/%]/.test(expression[expression.length - 1])) {
+            expression[expression.length - 1] = key;
+        } else {
+            expression.push(key)
+        }
+        numDispVal = "";
+        
     } else if (key == 'AC') {
         expression = [];
-        numDispLrg = "0";
+        numDispVal = "";
     } else if (key == '=' || key == 'Enter') {
-        expression.push(numDispLrg);
-        operate();
-        numDispLrg = '0';
+        if (numDispVal != '')
+            expression.push(numDispVal);
+            operate();
+            numDispVal = '';
     }
     
     // For updating the displays
-    if (expression.length == 0) {
-        updateDisplay(expressionDisp, '0');
-    } else {
-        updateDisplay(expressionDisp, expression.join(" "))
-    }
-    updateDisplay(numDisplay, numDispLrg)
+    updateDisplay()
+    console.log(`numDispVal:${numDispVal} \nExpression: ${expression}`)
 }
 
-function updateDisplay(display, info) {
-    display.innerHTML = info;
+function updateDisplay() {
+    if (expression.length == 0) {
+        expressionDisp.innerHTML = "0";
+    } else {
+        expressionDisp.innerHTML = expression.join(" ");
+    }
+    
+
+    if (numDispVal.length == 0) {
+        numDisplay.innerHTML = '0'
+    }
+    else {
+        numDisplay.innerHTML = numDispVal
+    }
 }
 
 function operate() {
     const specialChar = ['/', '%', '*', '+', '-']
-    
 
     for (let n = 0; n < specialChar.length; n++) {
         while (expression.indexOf(specialChar[n]) != -1) {
@@ -80,7 +98,6 @@ function operate() {
             let calcOperator = expression[index];
 
             endVal = calculate(parseFloat(oper1), calcOperator, parseFloat(oper2))
-            console.log(endVal)
             expression.splice((index-1), 3, endVal)
         }
     }
